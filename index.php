afb016f18652e83a9cf0f907d0adbbdeb2c999ec
Pierre <aodeltab@gmail.com>
2017-10-14 21:48:30 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+
+<html>
+
+
+<head>
+  <title>Master 2, Droit du Numérique - Caen</title>
+  <link rel='stylesheet' type='text/css' href='style/style.css'/>
+  <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel='stylesheet' href="https://www.w3schools.com/w3css/3/w3.css"
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <meta name='viewport' content="device-width, initial-scale=1">
+</head>
+
+
+<body>
+    <div class="header">
+      bouge le menu stp merci
+    </div>
+
+
+  <nav>
+    <div class="topnavT w3-content">
+      <a href="">ACCUEIL</a>
+      <a href="">PROMOTIONS</a>
+      <a href="">INTERVENANTS</a>
+      <a href="">ACTIVITES</a>
+    </div>
+  </nav>
+  <br>
+
+  <section class="w3-container w3-center w3-content" style="max-width:600px">
+    <h2 class="w3-wide">Master 2</h2>
+    <p class="w3-opacity"><i>Droit du Numérique - Caen</p>
+      <img src="img/logo.jpg"></img>
+    </section>
+  </body>
+
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+    <a href="#"><i class="fa fa-facebook-official"></i></a>
+    <a href="#"><i class="fa fa-linkedin"></i></a>
+    <p class="w3-medium">
+      Powered by <a href="http://google.fr/" target="_blank">Pierre Fortin</a>
+    </p>
+  </footer>
+
+
+
+  </html>
