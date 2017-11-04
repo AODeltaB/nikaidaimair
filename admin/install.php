df0e324b0d8081a5c21f98ad75f463eda90138d2
aodeltab <aodeltab@gmail.com>
2017-11-04 13:38:57 +0100

    Ajout page login

---
+++ admin/install.php
@@ -0,0 +1,11 @@
+<!-- /* Dans ce fichier il faut renseigner la base de données, l'utilisateur son mot de passe et l'hôte */ -->
+
+<?php
+
+$db_host = 'localhost';
+$db_user = 'root';
+$db_password = 'Pierre1998';
+$db_name = 'siteweb';
+
+
+ ?>
---
+++ admin/login.php
@@ -0,0 +1,101 @@
+<?php
+require 'config.php';
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<!-- HEAD -->
+<head>
+  <!-- <link rel="icon" href="logo.png" /> -->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <title>Master 2, Droit du Numérique - Caen</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+
+  <!-- CSS -->
+  <link href="../style/style.css" type='text/css' rel='stylesheet'>
+  <link href="../style/materialize.css" type="text/css" rel="stylesheet" media="screen,projection">
+  <link rel='stylesheet' href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel='stylesheet' href="https://www.w3schools.com/w3css/3/w3.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+</head>
+
+<!-- BODY -->
+<body>
+  <nav role="navigation">
+    <div class="nav-wrapper container">
+      <ul class="right hide-on-med-and-down">
+        <li><a href="../index.php">ACCUEIL</a></li>
+        <li><a href="../promotion.php">PROMOTION</a></li>
+        <li><a href="../intervenants.php">INTERVENANTS</a></li>
+        <li><a href="../activites.php">ACTIVITES</a></li>
+        <li><a href="../contact.php">CONTACTS</a></li>
+        <li><a href="../master.php">LE MASTER</a></li>
+      </ul>
+      <ul id="nav-mobile" class="side-nav">
+        <li><a href="../index.php">ACCUEIL</a></li>
+        <li><a href="../promotion.php">PROMOTION</a></li>
+        <li><a href="../intervenants.php">INTERVENANTS</a></li>
+        <li><a href="../activites.php">ACTIVITES</a></li>
+        <li><a href="../contact.php">CONTACTS</a></li>
+        <li><a href="../master.php">LE MASTER</a></li>
+      </ul>
+      <a href="#" data-activates="nav-mobile" class="button-collapse"><i class="material-icons">Menu</i></a>
+    </div>
+  </nav>
+
+  <div class="formulaireConnexion w3-container w3-center">
+    <h1>Administration du site</h1>
+    <p>Gestion des articles / membres / site en général.</p>
+    <form method="post" action="login.php">
+      <label>Email : </label>
+      <input type="email" name="email" placeholder="Votre Mail...">
+      <label>Mot de Passe</label>
+      <input type="password" name="password" placeholder="Votre mot de passe...">
+      <input type="submit" name="submit" value="Se connecter">
+    </form>
+  </div>
+
+
+  <!-- Footer -->
+  <footer class="w3-container w3-padding-64 w3-center w3-black w3-xlarge">
+    <a href="https://www.facebook.com/M2NumeriqueCaen/?fref=ts"><i class="fa fa-facebook-official"></i></a>
+    <a href="#"><i class="fa fa-linkedin"></i></a>
+    <a href="#"><i class="fa fa-instagram"></i></a>
+    <a href="#"><i class="fa fa-twitter"></i></a>
+    <p class="w3-medium">
+      Powered by <a href="https://www.linkedin.com/in/pierre-fortin-6b6908139/s" target="_blank">Pierre Fortin</a>
+    </p>
+  </footer>
+
+  <!-- Script  -->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src="../js/materialize.js"></script>
+  <script src="../js/init.js"></script>
+  <!-- <script src="../js/script.js"></script> -->
+
+</body>
+</html>
+
+<?php
+session_start();
+if(isset($_POST['submit']))
+{
+  $email = htmlentities($_POST['email']);
+  $password = htmlentities($_POST['password']);
+
+  if($email&&$password) {
+
+    $password = md5($password);
+    $connect = mysql_connect($db_host,$db_user,$db_password);
+    mysql_select_db($db_name);
+    $query = mysql_query("SELECT * FROM membres WHERE email='$email'&&password='$password'");
+    $rows = mysql_num_rows($query);
+    if ($rows==1){
+      // $_SESSION['pseudo']=$username;
+      // header('Location: ../index.php');
+      echo "Fonctionne";
+    }else echo "Pseudo ou Password incorrect";
+  } else echo"Veuillez saisir tous les champs";
+}
+?>
